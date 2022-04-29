ea907eeb43c4625e64037229e05936c5053bd4e6
snj11 <89026734+snj11@users.noreply.github.com>
2022-04-29 23:15:09 +0530

    Update DFID.py

--- DFID.py
+++ DFID.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[10]:
+# In[7]:
 
 
 def ret_value(k, var1, var = None):
@@ -12,33 +12,41 @@ def ret_value(k, var1, var = None):
           return key
 
 
-# In[11]:
+# In[8]:
 
 
-graph = {}
+graph = {
+    'A' : ['B', 'C',  'D'],#1
+    'C' : ['E', 'F'],#2
+    'G': ['H', 'J', 'I'],#3
+    'K' : [None],#4
+    'I' : ['K'],#5
+    'B' : ['D'],#6
+    'D' : ['L'],#7
+    'L' : ['A', 'M'],#8
+    'O' : ['N'],#9
+    'N' : ['T', 'H', 'Q'],#10
+    'H' : ['E'],#11
+    'P' : ['O'],#12
+    'Q' : ['H'],#13
+    'R' : ['Q'],#14
+    'S' : ['R'],#15
+    'J' : ['S'],#16
+    'U' : ['A', 'N', 'H'],#17
+    'T' : ['A', 'B'],#18
+    'E' : ['U', 'G'],#19
+    'F' : ['G', 'M'],#20
+    'M' : ['J', 'I'],#21
+}
 
-print("""INPUT INSTRUCTIONS
-1. Enter the graph as parent node and its children separated by spaces 
-(the first letter will be considered as the parent followed by the class("None" in case of a leaf node)):
-2. Enter the dist of each child node with the parent node separated by spaces: \n""")
-while True:
-    seq = input().split()
-    
-    if seq == ['-1']:
-        break
-        
-    edge = [seq[x+1] for x in range(len(seq)-1)]
-        
-    graph[seq[0]] = edge
+
+# In[10]:
 
 
 max_depth = int(input("\nEnter the maximum travesal depth : "))
 start_node = input("Enter start node: ")
 destn_node = input("Enter destination node: ")
-
-path = list()
-
-
+path = []
 def DFS(currentNode,destination,graph,maxDepth,curList):
     print("\n----- Next Iteration -----")
     print("Searching destination",currentNode)
@@ -70,4 +78,3 @@ if not iterativeDDFS(start_node, destn_node, graph, max_depth):
     print("\nPath unavailable!")
 else:
     print("\nA path exists!")
-
