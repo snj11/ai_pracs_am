a25de6251aa833efa869195e975623ec5604bef3
snj11 <89026734+snj11@users.noreply.github.com>
2022-05-01 08:30:52 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -51,3 +51,21 @@ def dfs(graph, start_node, end_node):
 start_node = input("Enter the starting node: ")
 end_node = input("Enter the ending node: ")
 dfs(graph, start_node, end_node)
+
+'''DFS(G, s, key):
+      stack = new Stack()
+      stack.push( s )            //Push s to stack 
+      mark s as visited
+      while ( stack is not empty):
+            //Pop node from stack and start to visit its children
+            v  =  stack.pop()
+            
+            if(v == key) return true //We found the key
+            
+            //Push all the unvisited neighbours of v to stack 
+            for all neighbours w of v in Graph G:
+                //unvisited neighbors
+                if w is not visited :   
+                     stack.push( w )         
+                     mark w as visited
+      return false'''
