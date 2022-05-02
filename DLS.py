0092b5499beb5b87c9245455236887131634f4a0
snj11 <89026734+snj11@users.noreply.github.com>
2022-05-02 08:22:02 +0530

    Update DLS.py

--- DLS.py
+++ DLS.py
@@ -36,18 +36,14 @@ if(res):
 else:
     print("No path available for the goal node in given depth limit")
 
-'''function DEPTH-LIMITED-SEARCH(problem, limit) returns a solution, or failure/cutoff
-    return RECURSIVE-DLS(MAKE-NODE(problem. INITIAL-STATE), problem, limit)
-
-function RECURSIVE-DLS(node, problem, limit) returns a solution, or failure/cutoff 
-    if problem.GOAL-TEST(node.STATE) then return SOLUTION(node) 
-    else if limit=0 then return cutoff
-    else 
-        cutoff-occurred? - false
-
-        for each action in problem.ACTIONS(node.STATE) do 
-            child CHILD-NODE(problem, node, action) 
-            result RECURSIVE-DLS(child, problem, limit - 1) 
-            if result = cutoff then cutoff_occurred? - true
-            else if result failure then return result
-        if cutoff occurred? then return cutoff else return failure'''
+'''booleanDLS(Node node, int limit, intdepth) 
+{ 
+if (depth > limit) return failure; 
+if (node is a goal node) return success; 
+for each child of node 
+ { 
+if (DLS(child, limit, depth + 1)) 
+ return success; 
+ } 
+ return failure; 
+} '''
